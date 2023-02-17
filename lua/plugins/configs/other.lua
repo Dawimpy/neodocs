d0736668bc64a6be758cd3b4afcf49bbc056b3de
Anonymous1231-dev <87801676+Anonymous1231-dev@users.noreply.github.com>
2023-02-17 16:01:57 +0800

    Fix autopairs
    Signed-off-by: Anonymous1231-dev <87801676+Anonymous1231-dev@users.noreply.github.com>

--- lua/plugins/configs/other.lua
+++ lua/plugins/configs/other.lua
@@ -6,6 +6,9 @@ M.autopairs = function ()
 		disable_filetype = { "TelescopePrompt", "vim" },
 	}
 
+	--Setup autopairs
+	require("nvim-autopairs").setup(options)
+
 	--Setup for cmp
 	local cmp_autopairs = require "nvim-autopairs.completion.cmp"
 	require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
